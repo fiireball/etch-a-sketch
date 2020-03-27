fc9042c532866bc8e2926e9d64181c8ab2ff4b92
fiireball <adrian.fiire@gmail.com>
2020-03-27 07:23:34 +0100

    Release 1.0

---
+++ readme.md
@@ -0,0 +1,11 @@
+This TOP-Projects aim is to build a sktechpad website.
+
+
+It should hopefully solidify my knowledge of JS-DOM manipulation.
+
+
+From: https://www.theodinproject.com/lessons/etch-a-sketch-project
+
+
+
+Results: TBD
---
+++ script.js
@@ -0,0 +1,92 @@
+function makeGrid(gridLength) {
+
+    const containerPad = document.querySelector('.container-pad')
+    const divSquare = document.createElement('div')
+
+
+
+    divSquare.classList.add('grid-square')
+
+    let gridSize = gridLength*gridLength;
+
+    containerPad.style.gridTemplateColumns = `repeat(${gridLength}, 1fr)`
+    containerPad.style.gridTemplateRows = `repeat(${gridLength}, 1fr)`
+
+
+    for (let i = 0; i < gridSize; i++) {
+        const divSquare = document.createElement('div')
+        divSquare.classList.add('grid-square')
+        containerPad.appendChild(divSquare);
+    }
+
+    addGridListeners()    
+}
+
+
+function randomColor() {
+    return Math.floor(Math.random()*255)
+}
+
+
+function addGridListeners() {
+    const gridSquares = document.querySelectorAll('.grid-square')
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.addEventListener('mouseenter', () => {
+            
+            if (gridSquare.style.backgroundColor == "") {
+                gridSquare.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
+            } else {
+                let currentColor = gridSquare.style.backgroundColor
+                gridSquare.style.backgroundColor = makeDarker(currentColor)
+            }
+
+
+            
+        })
+    })
+}
+
+
+function makeDarker(rgbString) {
+    // strip down rgb string into only the values
+    let rgbValues = rgbString.slice(rgbString.indexOf('(') + 1, rgbString.indexOf(')')).split(', ')
+
+    let rgbNumArray = [];
+
+    // Turn strings in array to numbers and reduce the values to 90%
+    for (let index = 0; index < rgbValues.length; index++) {
+        rgbNumArray.push(Math.floor(parseInt(rgbValues[index])*0.9-2))
+    }
+
+    let newRgbValues = `rgb(${rgbNumArray.join(', ')})`
+
+
+    return newRgbValues
+}
+
+
+function clearGrid() {
+    const oldGrid = document.querySelectorAll('.grid-square')
+    const containerPad = document.querySelector('.container-pad')
+    oldGrid.forEach(square => containerPad.removeChild(square))
+}
+
+
+newButton = document.querySelector('#button-new')
+resetButton = document.querySelector('#button-reset')
+
+
+newButton.onclick = () => {
+    newGridLength = prompt('How many grids long should the new pad be?')
+    clearGrid()
+    makeGrid(newGridLength)
+}
+
+
+resetButton.onclick = () => {
+    const currentGrid = document.querySelectorAll('.grid-square')
+    let currentLength = Math.sqrt(currentGrid.length)
+    clearGrid()
+    makeGrid(currentLength)
+}
+
